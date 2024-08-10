42085a8917fdc993194047f66a9d47bfbc21813d
rabbihossainlhp <rabbihossainlhp@gmail.com>
2024-08-10 16:02:36 +0600

    first commit running

---
+++ data.js
@@ -0,0 +1,22 @@
+export const imageData = [
+    {img_src: "./Images/First.jpg", name: "Chese"},
+    {img_src:"./Images/Second.jpg", name: "Leaf"},
+    {img_src:"./Images/Third.jpg",  name: "Key"},
+    {img_src:"./Images/Fourth.jpg", name: "Moon"},
+    {img_src:"./Images/Fifth.jpg",  name: "football"},
+    {img_src:"./Images/Sixth.jpg",  name: "football2"},
+    {img_src:"./Images/Seven.jpg",  name: "apple"},
+    {img_src:"./Images/Eight.jpg",  name: "butterfly"},
+
+
+    {img_src: "./Images/First.jpg", name: "Chese"},
+    {img_src:"./Images/Second.jpg", name: "Leaf"},
+    {img_src:"./Images/Third.jpg",  name: "Key"},
+    {img_src:"./Images/Fourth.jpg", name: "Moon"},
+    {img_src:"./Images/Fifth.jpg",  name: "football"},
+    {img_src:"./Images/Sixth.jpg",  name: "football2"},
+    {img_src:"./Images/Seven.jpg",  name: "apple"},
+    {img_src:"./Images/Eight.jpg",  name: "butterfly"}
+
+
+];
---
+++ index.js
@@ -0,0 +1,88 @@
+import {imageData} from"./data.js";
+let section = document.querySelector("section");
+let Moves = document.querySelector("section h2");
+let MoveCount = 7;
+Moves.innerText = MoveCount;
+
+
+//Get & Random data;
+const get_rand_Data = ()=>{
+    const getData = imageData;
+    const randData = getData.sort(()=>Math.random()-0.5);
+    
+    const mainDiv = document.createElement("div");
+    mainDiv.classList.add("mainDiv");
+    //Generate card with this images...
+    randData.forEach( (item)=>{
+        //create div/img/backpart......................
+        const singleCard = document.createElement("div");
+        const imgSide = document.createElement("img");
+        const backsideCard = document.createElement("div");
+        
+
+        //add class name...............................
+        singleCard.classList.add("singleCard");
+        imgSide.classList.add("imgSide");
+        backsideCard.classList.add("backsideCard");
+        
+        singleCard.setAttribute("name", item.name);
+
+        //add source into img.......................
+        imgSide.src = item.img_src
+
+
+        //add this two element in main Div.................... 
+        section.appendChild(mainDiv);
+
+        //Now append this all.........................
+        section.appendChild(mainDiv);
+        mainDiv.appendChild(singleCard);
+        singleCard.appendChild(imgSide)
+        singleCard.appendChild(backsideCard);
+
+
+        //now add eventlisten into the card............
+        singleCard.addEventListener("click",(e)=>{
+            singleCard.classList.toggle("toggleCard");
+            clickedCard(e);
+        })
+        
+        
+    })
+    //check is card clicked or not>>>
+    const clickedCard = (e)=>{
+        const checkTarget = e.target;
+        // console.log(checkTarget);
+
+        checkTarget.classList.add("flipped")
+        const getID = document.querySelectorAll(".flipped");
+    
+        //condition for set win/loss by simple logic........
+        if(getID.length === 2){
+            if(getID[0].getAttribute("name") === getID[1].getAttribute("name")){
+                getID.forEach((card)=>{
+                    card.classList.remove("flipped");
+                    card.style.pointerEvents = "none";
+                })
+                console.log("congratch");
+            }else{
+                getID.forEach((card)=>{
+                    card.classList.remove("flipped");
+                    setTimeout(()=>{
+                        card.classList.remove("toggleCard");
+                    },1000)
+                    
+                    MoveCount--;
+                    Moves.innerText = MoveCount;
+
+                    console.log(card)
+                })
+            }  
+        }
+    }
+}
+
+
+
+
+get_rand_Data();
